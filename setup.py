ec3686077b29d43830e91e7b7e1ea09718ae2463
uyuutosa <sayu819@gmail.com>
2020-02-06 18:40:20 +0900

    add pytest to setup.pyq

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="size_recommender",
-    version="0.0.1",
-    install_requires=["pandas", "numpy"],
+    version="0.0.2",
+    install_requires=["pandas", "numpy", "pytest"],
     author="uyuutosa",
     auther_email="yu@trait.llc",
     license="MIT",
