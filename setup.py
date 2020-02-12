163c591023af5df5fc71722bafc45178e8f4d8e1
uyuutosa <yu@originalstitch.com>
2020-02-12 17:29:17 +0900

    rev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="size_recommender",
-    version="0.0.3",
+    version="0.0.4",
     install_requires=["pandas", "numpy", "pytest", "xlrd"],
     author="uyuutosa",
     auther_email="yu@trait.llc",
--- size_recommender/size_recommender.py
+++ size_recommender/size_recommender.py
@@ -7,16 +7,10 @@ class SizeRecommender:
     def  __init__(self, path):
         self.f = pd.ExcelFile(path)
     def _recommend(self, df, param_dic, required_param_lst):
-        c_max = where(["max" in x for x in df.columns ])[0][-1]
-        idx_lst = []
-        param_name_lst = []
-        for i in range(0, c_max+1, 2):
-            n_min, n_max = df.columns[i:i+2]
-            result = df[(df[n_min] <= param_dic[n_min.strip("_min")]) == (df[n_max] > param_dic[n_min.strip("_min")])]
-            if result.shape[0]:
-                idx_lst += [result.index[0]]
-                param_name_lst += [n_min.strip("_min")]
-    
+        param_name_lst, idx_lst = self._commonPartRecommend(df, param_dic, required_param_lst)
+        if len(idx_lst) == 0:
+            param_name_lst, idx_lst = self._mostFrequentRecommend(df, param_dic, required_param_lst)
+
         # extract most frequent value
         a,b = unique(idx_lst, return_counts=True)
         best_dic = {}
@@ -30,6 +24,34 @@ class SizeRecommender:
             all_dic[name] = tmp_dic
 
         return {"best_dic": best_dic, "all_dic": all_dic}
+        
+    def _commonPartRecommend(self, df, param_dic, required_param_lst):
+        c_max = where(["max" in x for x in df.columns ])[0][-1]
+        idx_lst = []
+        param_name_lst = []
+        for i in range(0, c_max+1, 2):
+            n_min, n_max = df.columns[i:i+2]
+            df = df[(df[n_min] <= param_dic[n_min.strip("_min")]) == (df[n_max] > param_dic[n_min.strip("_min")])]
+            if df.shape[0]:
+                param_name_lst += [n_min.strip("_min")]
+        if df.shape[0]:
+            idx_lst += [df.index[0]] * len(param_name_lst)
+
+        return param_name_lst, idx_lst
+            
+
+    def _mostFrequentRecommend(self, df, param_dic, required_param_lst):
+        c_max = where(["max" in x for x in df.columns ])[0][-1]
+        idx_lst = []
+        param_name_lst = []
+        for i in range(0, c_max+1, 2):
+            n_min, n_max = df.columns[i:i+2]
+            result = df[(df[n_min] <= param_dic[n_min.strip("_min")]) == (df[n_max] > param_dic[n_min.strip("_min")])]
+            if result.shape[0]:
+                idx_lst += [result.index[0]]
+                param_name_lst += [n_min.strip("_min")]
+        return param_name_lst, idx_lst
+    
             
     def _get_df(self, sheet_name):
         df = self.f.parse(sheet_name)
@@ -49,9 +71,22 @@ def data_male():
 def test_male(data_male):
     sr = SizeRecommender(path="assets/size_chart.xlsx")
     truth_value = {"best_dic": {"size": "M"},
-                   "all_dic": {"Height": {"size":"M"}, "Neck": {"size": "M"}, "Chest": {"size":"M"}, "Waist": {"size":"M"}, "Hip": {"size":"S"}}
+                   "all_dic": {"Height": {"size":"M"}, "Neck": {"size": "M"}, "Chest": {"size":"M"}, "Waist": {"size":"M"}, "Hip": {"size":"M"}}
                    }
     assert sr.recommend(data_male["param_dic"], "male", ["size"]) == truth_value
+
+@pytest.fixture()
+def data_male2():
+    param_dic = {"Height": 160, "Neck":39, "Chest": 100, "Waist":110, "Hip": 98}
+    ret = {"param_dic": param_dic}
+    return ret
+
+def test_male2(data_male2):
+    sr = SizeRecommender(path="assets/size_chart.xlsx")
+    truth_value = {"best_dic": {"size": "L"},
+                   "all_dic": {"Height": {"size":"XS"}, "Neck": {"size": "M"}, "Chest": {"size":"L"}, "Waist": {"size":"3XL"}, "Hip": {"size":"L"}}
+                   }
+    assert sr.recommend(data_male2["param_dic"], "male", ["size"]) == truth_value
 
 @pytest.fixture()
 def data_female():
@@ -62,6 +97,19 @@ def data_female():
 def test_female(data_female):
     sr = SizeRecommender(path="assets/size_chart.xlsx")
     truth_value = {"best_dic": {"size": "L"},
-                    "all_dic": {"Height": {"size":"M"}, "Chest": {"size":"L"}, "Waist": {"size":"L"}, "Hip": {"size":"L"}}
+                    "all_dic": {"Height": {"size":"L"}, "Chest": {"size":"L"}, "Waist": {"size":"L"}, "Hip": {"size":"L"}}
                    }
     assert sr.recommend(data_female["param_dic"], "female", ["size"]) == truth_value
+
+@pytest.fixture()
+def data_female2():
+    param_dic = {"Height": 152, "Chest": 90, "Waist":70, "Hip": 115}
+    ret = {"param_dic": param_dic}
+    return ret
+
+def test_female2(data_female2):
+    sr = SizeRecommender(path="assets/size_chart.xlsx")
+    truth_value = {"best_dic": {"size": "L"},
+                    "all_dic": {"Height": {"size":"XS"}, "Chest": {"size":"L"}, "Waist": {"size":"L"}, "Hip": {"size":"3XL"}}
+                   }
+    assert sr.recommend(data_female2["param_dic"], "female", ["size"]) == truth_value
